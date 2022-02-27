e7d312386bc3e723ff790ddeccc9e8b04522d465
harishtalluru <harshatalluru20@gmail.com>
2022-02-27 16:17:49 +0530

    first commit

--- react_form-master/src/components/loginpage/login.js
+++ react_form-master/src/components/loginpage/login.js
@@ -59,7 +59,7 @@ render(){
     <input id='input-text' type="text"  value={this.state.email}
             onChange={this.onEmailChange} placeholder="Email Address*" size="40" />
     <input id='input-psw' type="password" value={this.state.password} onChange={this.onPasswordChange} placeholder="Password*" size="40" />
-   <input id='input1' type="submit" value="Login" size="40" />
+   <input id='input1' type="submit" value="Login"  />
    <br/>
    
 
